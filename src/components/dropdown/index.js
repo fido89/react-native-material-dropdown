3455b772ae2705441250284e8fa651f36cbbbead
Denis <d.fidersek@gmail.com>
2020-10-02 21:39:48 +0200

    Update compatibilty to expo sdk 38

--- src/components/dropdown/index.js
+++ src/components/dropdown/index.js
@@ -75,7 +75,7 @@ export default class Dropdown extends PureComponent {
       'landscape-right',
     ],
 
-    useNativeDriver: false,
+    useNativeDriver: true,
   };
 
   static propTypes = {
@@ -152,7 +152,7 @@ export default class Dropdown extends PureComponent {
 
     supportedOrientations: PropTypes.arrayOf(PropTypes.string),
 
-    useNativeDriver: PropTypes.bool,
+    useNativeDriver: PropTypes.bool.isRequired,
   };
 
   constructor(props) {
@@ -299,7 +299,7 @@ export default class Dropdown extends PureComponent {
             .timing(opacity, {
               duration: animationDuration,
               toValue: 1,
-              useNativeDriver,
+              useNativeDriver: true,
             })
             .start(() => {
               if (this.mounted && 'ios' === Platform.OS) {
@@ -323,7 +323,7 @@ export default class Dropdown extends PureComponent {
       .timing(opacity, {
         duration: animationDuration,
         toValue: 0,
-        useNativeDriver,
+        useNativeDriver: true,
       })
       .start(() => {
         this.focused = false;
